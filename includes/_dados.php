9fdf100353025014347d1171f5bd0bfe20a4c880
arthurw26 <78936078+arthurw26@users.noreply.github.com>
2022-06-02 12:27:53 -0300

    2 comit
    adiciona as arrays e as nfts

---
+++ includes/_dados.php
@@ -0,0 +1,21 @@
+<?php
+
+$produtos = array{
+    0 => array('nome'=> 'nft capivara','preco'=>'1200.00', 'descricao'=>' Descrição nft capivara', 'imagem'=> 'nft-capivara.jpg'), 
+    1 => array('nome'=> 'nft neymar','preco'=>'50000.00', 'descricao'=>' Descrição nft capivara', 'imagem'=> 'nft-capivara.jpg'),
+    2 => array('nome'=> 'nft capivara','preco'=>'1200.00', 'descricao'=>' Descrição nft capivara', 'imagem'=> 'nft-capivara.jpg'),
+    3 => array('nome'=> 'nft capivara','preco'=>'1200.00', 'descricao'=>' Descrição nft capivara', 'imagem'=> 'nft-capivara.jpg'),
+    4 => array('nome'=> 'nft capivara','preco'=>'1200.00', 'descricao'=>' Descrição nft capivara', 'imagem'=> 'nft-capivara.jpg'),
+    5 => array('nome'=> 'nft capivara','preco'=>'1200.00', 'descricao'=>' Descrição nft capivara', 'imagem'=> 'nft-capivara.jpg'),
+    6 => array('nome'=> 'nft capivara','preco'=>'1200.00', 'descricao'=>' Descrição nft capivara', 'imagem'=> 'nft-capivara.jpg'),
+    7 => array('nome'=> 'nft capivara','preco'=>'1200.00', 'descricao'=>' Descrição nft capivara', 'imagem'=> 'nft-capivara.jpg'),
+    8 => array('nome'=> 'nft capivara','preco'=>'1200.00', 'descricao'=>' Descrição nft capivara', 'imagem'=> 'nft-capivara.jpg'),
+    9 => array('nome'=> 'nft capivara','preco'=>'1200.00', 'descricao'=>' Descrição nft capivara', 'imagem'=> 'nft-capivara.jpg'),
+
+
+
+}
+    
+
+
+?>
