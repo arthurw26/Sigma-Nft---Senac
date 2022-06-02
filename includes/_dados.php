0c74d6356c00ec771a07ff6a806ee0701c99f39f
arthurw26 <arthur.wiatrowski@gmail.com>
2022-06-02 15:36:23 -0300

    termina array

--- includes/_dados.php
+++ includes/_dados.php
@@ -2,15 +2,15 @@
 
 $produtos = array{
     0 => array('nome'=> 'nft capivara','preco'=>'1200.00', 'descricao'=>' Descrição nft capivara', 'imagem'=> 'nft-capivara.jpg'), 
-    1 => array('nome'=> 'nft neymar','preco'=>'50000.00', 'descricao'=>' Descrição nft capivara', 'imagem'=> 'nft-capivara.jpg'),
-    2 => array('nome'=> 'nft capivara','preco'=>'1200.00', 'descricao'=>' Descrição nft capivara', 'imagem'=> 'nft-capivara.jpg'),
-    3 => array('nome'=> 'nft capivara','preco'=>'1200.00', 'descricao'=>' Descrição nft capivara', 'imagem'=> 'nft-capivara.jpg'),
-    4 => array('nome'=> 'nft capivara','preco'=>'1200.00', 'descricao'=>' Descrição nft capivara', 'imagem'=> 'nft-capivara.jpg'),
-    5 => array('nome'=> 'nft capivara','preco'=>'1200.00', 'descricao'=>' Descrição nft capivara', 'imagem'=> 'nft-capivara.jpg'),
-    6 => array('nome'=> 'nft capivara','preco'=>'1200.00', 'descricao'=>' Descrição nft capivara', 'imagem'=> 'nft-capivara.jpg'),
-    7 => array('nome'=> 'nft capivara','preco'=>'1200.00', 'descricao'=>' Descrição nft capivara', 'imagem'=> 'nft-capivara.jpg'),
-    8 => array('nome'=> 'nft capivara','preco'=>'1200.00', 'descricao'=>' Descrição nft capivara', 'imagem'=> 'nft-capivara.jpg'),
-    9 => array('nome'=> 'nft capivara','preco'=>'1200.00', 'descricao'=>' Descrição nft capivara', 'imagem'=> 'nft-capivara.jpg'),
+    1 => array('nome'=> 'nft neymar','preco'=>'50000.00', 'descricao'=>' Descrição nft neymar', 'imagem'=> 'nft-neymar.jpg'),
+    2 => array('nome'=> 'nft cachorro','preco'=>'10000.00', 'descricao'=>' Descrição nft cachorro', 'imagem'=> 'nft-cachorro-.jpg'),
+    3 => array('nome'=> 'nft gorila','preco'=>'1500.00', 'descricao'=>' Descrição nft gorila', 'imagem'=> 'nft-gorila.jpg'),
+    4 => array('nome'=> 'nft leao','preco'=>'1200.00', 'descricao'=>' Descrição nft leao', 'imagem'=> 'nft-leao.jpg'),
+    5 => array('nome'=> 'nft pato','preco'=>'1200.00', 'descricao'=>' Descrição nft pato', 'imagem'=> 'nft-pato.jpg'),
+    6 => array('nome'=> 'nft crocodilo','preco'=>'1200.00', 'descricao'=>' Descrição nft crocodilo', 'imagem'=> 'nft-crocodilo.jpg'),
+    7 => array('nome'=> 'nft panda','preco'=>'1200.00', 'descricao'=>' Descrição nft panda', 'imagem'=> 'nft-panda.jpg'),
+    8 => array('nome'=> 'nft rato','preco'=>'1200.00', 'descricao'=>' Descrição nft rato', 'imagem'=> 'nft-rato.jpg'),
+    9 => array('nome'=> 'nft macaco','preco'=>'1200.00', 'descricao'=>' Descrição nft macaco', 'imagem'=> 'nft-macaco.jpg'),
 
 
 
