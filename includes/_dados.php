23e943efb80d6e777b8f817c9a61c04611592892
arthurw26 <78936078+arthurw26@users.noreply.github.com>
2022-06-09 12:05:04 -0300

    sdsadsa

--- includes/_dados.php
+++ includes/_dados.php
@@ -1,9 +1,9 @@
 <?php
 
-$produtos = array{
+$produtos = array(
     0 => array('nome'=> 'nft capivara','preco'=>'1200.00', 'descricao'=>' Descrição nft capivara', 'imagem'=> 'nft-capivara.jpg'), 
     1 => array('nome'=> 'nft neymar','preco'=>'50000.00', 'descricao'=>' Descrição nft neymar', 'imagem'=> 'nft-neymar.jpg'),
-    2 => array('nome'=> 'nft cachorro','preco'=>'10000.00', 'descricao'=>' Descrição nft cachorro', 'imagem'=> 'nft-cachorro-.jpg'),
+    2 => array('nome'=> 'nft gremio','preco'=>'10000.00', 'descricao'=>' Descrição nft gremio', 'imagem'=> 'nft-gremio.jpg'),
     3 => array('nome'=> 'nft gorila','preco'=>'1500.00', 'descricao'=>' Descrição nft gorila', 'imagem'=> 'nft-gorila.jpg'),
     4 => array('nome'=> 'nft leao','preco'=>'1200.00', 'descricao'=>' Descrição nft leao', 'imagem'=> 'nft-leao.jpg'),
     5 => array('nome'=> 'nft pato','preco'=>'1200.00', 'descricao'=>' Descrição nft pato', 'imagem'=> 'nft-pato.jpg'),
@@ -11,10 +11,10 @@ $produtos = array{
     7 => array('nome'=> 'nft panda','preco'=>'1200.00', 'descricao'=>' Descrição nft panda', 'imagem'=> 'nft-panda.jpg'),
     8 => array('nome'=> 'nft rato','preco'=>'1200.00', 'descricao'=>' Descrição nft rato', 'imagem'=> 'nft-rato.jpg'),
     9 => array('nome'=> 'nft macaco','preco'=>'1200.00', 'descricao'=>' Descrição nft macaco', 'imagem'=> 'nft-macaco.jpg'),
+)
 
 
 
-}
     
 
 
--- index.php
+++ index.php
@@ -8,6 +8,21 @@ include_once './includes/_header.php';
 <h1>Home</h1>
 
 <?php
+ for ($i=0; $i <3 ; $i++) { 
+     echo $i. '<br>';
+     echo $produtos [$i]{'nome'};
+     echo $produtos [$i]{'preco'};
+     echo $produtos [$i]{'imagem'};
+     ?>
+     <img src="./content/<?php echo $produtos[$i]['imagem'];?>" width='200px'> <?php
+     echo '<hr>';
+ }
+
+?>
+
+
+
+<?php
 // include do footer
 include_once './includes/_footer.php';
 ?>
