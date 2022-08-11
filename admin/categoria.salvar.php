e0000e377ac46e68938fad125a7ad2a44985692b
Arthur da Silva Wiatrowski <78936078+arthurw26@users.noreply.github.com>
2022-08-11 12:25:46 -0300

    painel admin

--- admin/categoria.salvar.php
+++ admin/categoria.salvar.php
@@ -1,8 +1,28 @@
 <?php
 include_once '../includes/_banco';
 include_once './_header.php';
-include_once  './menu.php';
+if (isset($_GET['id']) || !empty($_GET['id'])){
+    $id = $_GET['id'];
+    $sql = "SELECT * FROM categorias WHERE CategoriaID =".$id;
+    $dados = mysqli_fetch_array($resultado,MYSQLI_ASSOC);
+}
+    else{
+        $id = '';
+        $dados['Nome'] = '';
+        $dados['Descrição'] = '';
+        $dados['Imagem'] = ''; 
+    }
+    include_once  './menu.php';
 ?>
+    <main>
+        <h2>Adminstração das Categorias</h2>
+        <a href="categoria-lista.php">Listagem</a>
+        <hr>
+        <form action="categoria-processa.php" method="post" enctype="multipart/form-data">
+            <input type="hidden" value="salvar" name="acao">
+        
+        </form>
+    </main>
 
 
 
